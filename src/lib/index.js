41b9654a115ec622ec63bf07aba01cf976a63a7b
Ludwig Muench <muench.ludwig@gmail.com>
2018-06-02 22:27:28 +0200

    delete lib folder

--- src/lib/index.js
+++
@@ -1,9 +0,0 @@
-import LightLink from './components/LightLink';
-import LightRouter from './components/LightRouter';
-import LightUrl from './components/LightUrl';
-
-export {
-  LightLink,
-  LightRouter,
-  LightUrl
-};
