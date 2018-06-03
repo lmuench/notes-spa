adbd5ff8324d99e6430d93449393e55b267843d6
Ludwig Muench <muench.ludwig@gmail.com>
2018-06-03 11:58:17 +0200

    implement Notebooks component

--- src/components/Notebooks.js
+++ src/components/Notebooks.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-const Notebooks = () => (
-  <div>
-    Notebooks
-  </div>
-);
+class Notebooks extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notebooks: []
+    };
+  }
+
+  componentDidMount = async () => {
+    const notebooks = await this.getNotebooks();
+    this.setState({ notebooks: notebooks });
+  };
+
+  getNotebooks = async () => {
+    const response = await fetch('http://localhost:5000/notebooks');
+    const json = await response.json();
+    return json;
+  };
+
+  notebookTable = () => (
+    <table>
+      <this.notebookTableRows />
+    </table>
+  );
+
+  notebookTableRows = () => (
+    this.state.notebooks.map(notebook => (
+      <tr>
+        <td>{notebook.title}</td>
+      </tr>
+    ))
+  );
+
+  render = () => (
+    <this.notebookTable />
+  );
+}
 
 export default Notebooks;
